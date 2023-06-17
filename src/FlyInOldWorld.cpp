a769342363bde31020a093ff7b73c17037622759
Tralenor <tralenor@gmail.com>
2023-06-17 20:43:57 +0200

    add debug message

--- src/FlyInOldWorld.cpp
+++ src/FlyInOldWorld.cpp
@@ -17,15 +17,19 @@ public:
 
     bool OnCanPlayerFlyInZone(const Player* player, uint32 mapId, uint32 zoneId, SpellInfo const* bySpell) override
     {
+        player->GetSession()->SendAreaTriggerMessage("%s", "Hook executed");
         uint32 v_map = GetVirtualMapForMapAndZone(mapId, zoneId);
         if (v_map == 0 || v_map == 1)
         {
+            player->GetSession()->SendAreaTriggerMessage("%s", "Hook in vMap = 0/1");
             if (!player->HasSpell(OLD_WORLD_FLYING_SPELL)) // 54197 = Cold Weather Flying
             {
+                player->GetSession()->SendAreaTriggerMessage("%s", "Hook: has no Spell");
                 return false;
             }
+            
         }
-
+        player->GetSession()->SendAreaTriggerMessage("%s", "Hook: end reached - return true");
         return true;
     }
 };
